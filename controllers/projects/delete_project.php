da9f913d9694c40871477306ce28848b4b28b14d
ElFirdaous28 <elmokhtarifirdaous@gmqil.com>
2024-12-21 21:36:44 +0100

    GDCI-11 pages access fixing

---
+++ controllers/projects/delete_project.php
@@ -0,0 +1,17 @@
+<?php
+    include '../connection.php';
+    // function to remove project
+    function removeProject($idProject,$conn){
+        $removeProject = $conn->prepare("DELETE FROM projets WHERE id_projet=?");
+        $removeProject->execute([$idProject]);
+    }
+    
+    // check the post request to remove the user
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['remove_project'])) {
+        $idUser = $_POST['id_projet'];
+        removeProject($idUser,$conn);
+        // Redirect to avoid form resubmission after page reload
+        header("Location: my_projects.php");
+        exit();
+    }
+?>
--- test.php
+++ test.php
@@ -1,28 +1,5 @@
-<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-    <div class="text-sm leading-5 text-gray-900 w-full"><?= $user['title'] !== null ? htmlspecialchars($user['title']) : ''; ?></div>
-</td>
-
-<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-    <form method="POST" style="display:inline;" onsubmit="return confirm('Are you sure you want to change the status of this user?');">
-        <input type="hidden" name="block_user_id" value="<?= $user['id_utilisateur']; ?>">
-        <button type="submit" class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">
-            <?= $user['is_active'] == 1 ? "Active" : "blocked" ?>
-        </button>
-    </form>
-</td>
-
-<td class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
-    <?= $user['role'] == 2 ? "Client" : "Freelancer"; ?>
-</td>
-
-<td class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
-    <?= $user['role'] == 2 ? "Client" : "Freelancer"; ?>
-</td>
-
-<td class="px-6 py-4 text-sm font-medium leading-5 text-right whitespace-no-wrap border-b border-gray-200">
-    <!-- Remove User Form with Confirmation -->
-    <form method="POST" style="display:inline;" onsubmit="return confirm('Are you sure you want to remove this user?');">
-        <input type="hidden" name="remove_user" value="<?= $user['id_utilisateur']; ?>">
-        <button type="submit" class="text-indigo-600 hover:text-indigo-900">Remove</button>
-    </form>
-</td>
+<?php
+$currentPath = $_SERVER['REQUEST_URI'];
+echo $currentPath;
+die();
+?>
